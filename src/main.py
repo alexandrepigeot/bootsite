5db465e1836489db6e4d728224c08a352a11b2a4
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-07-30 17:26:51 +0100

    CH2L1 made TextNode

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import TextNode, TextType
+
+
+test = TextNode("Some text", TextType.PLAIN, "http://localhost")
+
+print(test)
---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from enum import Enum
+
+
+class TextType(Enum):
+	PLAIN = "plain"
+	BOLD = "bold"
+	ITALIC = "italic"
+	CODE = "code"
+	LINK = "link"
+	IMAGE = "image"
+
+class TextNode:
+	def __init__(self, text, text_type, url = None) -> None:
+		self.text = text
+		self.text_type = text_type
+		self.url = url
+
+	def __eq__(self, other: object) -> bool:
+		if not isinstance(other, TextNode):
+			return False
+
+		return (self.text, self.text_type, self.url) == (other.text, other.text_type, other.url)
+
+	def __repr__(self) -> str:
+		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
