bc0ceda42c52e3a17aa434ce23e7cf6bf0fc2d66
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-08-02 09:30:53 +0100

    specified types in constructor

--- src/textnode.py
+++ src/textnode.py
@@ -4,24 +4,29 @@ from enum import Enum
 
 
 class TextType(Enum):
-	PLAIN = "plain"
-	BOLD = "bold"
-	ITALIC = "italic"
-	CODE = "code"
-	LINK = "link"
-	IMAGE = "image"
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
 
 class TextNode:
-	def __init__(self, text, text_type, url = None) -> None:
-		self.text = text
-		self.text_type = text_type
-		self.url = url
+    def __init__(self, text, text_type, url=None) -> None:
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
 
-	def __eq__(self, other: object) -> bool:
-		if not isinstance(other, TextNode):
-			return False
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
 
-		return (self.text, self.text_type, self.url) == (other.text, other.text_type, other.url)
+        return (self.text, self.text_type, self.url) == (
+            other.text,
+            other.text_type,
+            other.url,
+        )
 
-	def __repr__(self) -> str:
-		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
