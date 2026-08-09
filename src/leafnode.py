b12062ddf63bdfcdce2eb64e389af088fd30b580
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-08-09 16:26:12 +0100

    CH2L4 Made Leaf Nodes

---
+++ src/leafnode.py
@@ -0,0 +1,18 @@
+from src.htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if self.value is None or self.value == "":
+            raise ValueError("Leaf Node must have a value")
+
+        if self.tag is None or self.tag == "":
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
---
+++ tests/test_leafnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from src.leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_generic_node(self):
+        tag: str = "p"
+        text: str = "I'm a text"
+        node: LeafNode = LeafNode(tag=tag, value=text)
+
+        self.assertEqual(f"{node}", f"LeafNode({tag}, {text}, None)")
+        self.assertEqual(node.to_html(), f"<{tag}>{text}</{tag}>")
+
+    def test_node_with_props(self):
+        tag: str = "p"
+        text: str = "I'm a text"
+        props: dict[str, str] = {"href": "http://boot.dev", "a": "http://boot.dev"}
+        node: LeafNode = LeafNode(tag=tag, value=text, props=props)
+
+        self.assertEqual(node.__repr__(), f"LeafNode({tag}, {text}, {props})")
+        self.assertEqual(
+            node.to_html(),
+            f'<{tag} href="{props["href"]}" a="{props["a"]}">{text}</{tag}>',
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
