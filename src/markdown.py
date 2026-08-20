064f3eb69ea29bea6689337ca17678bd8918e0cd
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-08-20 10:36:34 +0100

    CH3L3 We have extraction!!!

--- src/markdown.py
+++ src/markdown.py
@@ -1,4 +1,68 @@
+from __future__ import annotations
+
+import re
+from typing import override
+
 from src.textnode import TextNode, TextType
+
+IMAGE_PATTERN: str = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+LINK_PATTERN: str = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+
+
+class ImageNode:
+    def __init__(self, alt: str, src: str) -> None:
+        self.alt: str = alt
+        self.src: str = src
+
+    @override
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, ImageNode):
+            return False
+
+        return (self.alt, self.src) == (other.alt, other.src)
+
+    @override
+    def __repr__(self) -> str:
+        return f"ImageNode({self.alt}, {self.src})"
+
+
+class LinkNode:
+    def __init__(self, text: str, url: str) -> None:
+        self.text: str = text
+        self.url: str = url
+
+    @override
+    def __eq__(self, other: object, /) -> bool:
+        if not isinstance(other, LinkNode):
+            return False
+
+        return (self.text, self.url) == (other.text, other.url)
+
+    @override
+    def __repr__(self) -> str:
+        return f"LinkNode({self.text}, {self.url})"
+
+
+def extract_images(text: str) -> list[ImageNode]:
+    matches: list[(str)] = re.findall(IMAGE_PATTERN, text)
+
+    result: list[ImageNode] = []
+
+    for match in matches:
+        result.append(ImageNode(match[0], match[1]))
+
+    return result
+
+
+def extract_links(text: str) -> list[LinkNode]:
+    matches: list[(str)] = re.findall(LINK_PATTERN, text)
+
+    result: list[LinkNode] = []
+
+    for match in matches:
+        result.append(LinkNode(match[0], match[1]))
+
+    return result
 
 
 def split_nodes(
--- tests/test_markdown.py
+++ tests/test_markdown.py
@@ -1,16 +1,40 @@
 import unittest
 
-from src.markdown import split_nodes
+from src.markdown import ImageNode, LinkNode, extract_images, split_nodes
 from src.textnode import TextNode, TextType
 
 
 class TestMarkdown(unittest.TestCase):
-    def test_split_text_node(self):
+    def test_image_node(self):
+        node1: ImageNode = ImageNode("alternate text", "source url")
+        node2: ImageNode = ImageNode("alternate text", "source url")
+
+        self.assertEqual("ImageNode(alternate text, source url)", node1.__repr__())
+        self.assertEqual(node1, node2)
+
+    def test_link_node(self):
+        node1: LinkNode = LinkNode(text="This is text", url="http://boot.dev")
+        node2: LinkNode = LinkNode(text="This is text", url="http://boot.dev")
+
+        self.assertEqual("LinkNode(This is text, http://boot.dev)", node1.__repr__())
+        self.assertEqual(node1, node2)
+
+    def test_extract_images(self) -> None:
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        extracted_nodes: list[ImageNode] = extract_images(text)
+        expected_nodes: list[ImageNode] = [
+            ImageNode(alt="rick roll", src="https://i.imgur.com/aKaOqIh.gif"),
+            ImageNode(alt="obi wan", src="https://i.imgur.com/fJRm4Vk.jpeg")
+        ]
+
+        self.assertEqual(expected_nodes, extracted_nodes)
+
+    def test_split_text_node(self) -> None:
         node: TextNode = TextNode(text="This is a text", text_type=TextType.TEXT)
 
         self.assertEqual([node], split_nodes([node], "**", TextType.BOLD))
 
-    def test_split_bold_node(self):
+    def test_split_bold_node(self) -> None:
         node: TextNode = TextNode(
             text="This is a **bold** text", text_type=TextType.TEXT
         )
@@ -24,7 +48,7 @@ class TestMarkdown(unittest.TestCase):
             split_nodes([node], "**", TextType.BOLD),
         )
 
-    def test_split_complex_bold_nodes(self):
+    def test_split_complex_bold_nodes(self) -> None:
         nodes: list[TextNode] = [
             TextNode(text="**bold**", text_type=TextType.TEXT),
             TextNode(text="This is **bold**", text_type=TextType.TEXT),
@@ -42,7 +66,7 @@ class TestMarkdown(unittest.TestCase):
             split_nodes(old_nodes=nodes, delimiter="**", text_type=TextType.BOLD),
         )
 
-    def test_split_invalid_node(self):
+    def test_split_invalid_node(self) -> None:
         node: TextNode = TextNode(text="**", text_type=TextType.TEXT)
 
         self.assertRaises(SyntaxError, split_nodes, [node], "**", TextType.BOLD)
