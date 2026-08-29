055837badb2a25e2182d425ec62f979573c393c8
Alexandre Pigeot <alexandrepigeot@gmail.com>
2026-08-29 08:01:16 +0100

    CH5L3 We can generate pages now

--- src/main.py
+++ src/main.py
@@ -4,3 +4,15 @@ from webpage import generate_page
 files_move_static()
 
 generate_page("content/index.md", "public/index.html", "template.html")
+generate_page(
+    "content/blog/glorfindel/index.md",
+    "public/blog/glorfindel/index.html",
+    "template.html",
+)
+generate_page(
+    "content/blog/majesty/index.md", "public/blog/majesty/index.html", "template.html"
+)
+generate_page(
+    "content/blog/tom/index.md", "public/blog/tom/index.html", "template.html"
+)
+generate_page("content/contact/index.md", "public/contact/index.html", "template.html")
